dad2d6bbb355b5e476023330b106398542d2b799
i9 <oluwarinolasam@gmail.com>
2024-02-02 00:01:09 +0100

    actions and loaders into main.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ export default function Logo() {
 	return (
 		<p
 			style={{ fontFamily: "Foco" }}
-			className="logo text-2xl font-bold scale-150"
+			className="logo text-3xl font-bold scale-150"
 		>
 			<span className="text-prim">i9X</span>
 			<span className="text-gray-900 text-sm">mas</span>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Root from './components/Root'
-import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import AppStateStore from './lib/appState/store'
 import { Provider as ReduxProvider } from 'react-redux'
+
+import AppStateStore from './lib/appState/store'
+import * as Actions from './lib/actions'
+import * as Loaders from './lib/loaders'
+
+import './index.css'
+import Root from './components/Root'
 import App from './components/App'
 import Auth from './components/auth/Auth'
 import Home from './pages/Home'
@@ -13,7 +17,7 @@ import CreatePost from './pages/CreatePost'
 import Signin from './pages/auth/Signin'
 import Signup from './pages/auth/Signup'
 import VerifyEmail from './pages/auth/VerifyEmail'
-import ProfileInfo from './pages/auth/ProfileInfo'
+import NewAccountInfo from './pages/auth/NewAccountInfo'
 import UploadProfilePicture from './pages/auth/UploadProfilePicture'
 import ConfirmPwdrEmail from './pages/auth/ConfirmPwdrEmail'
 import ResetPassword from './pages/auth/ResetPassword'
@@ -46,18 +50,22 @@ const router = createBrowserRouter([
         children: [
           {
             path: "/auth/signup",
-            element: <Signup />
+            element: <Signup />,
+            action: Actions.newAccountRequest,
           },
           {
-            path: "/auth/verify_email",
-            element: <VerifyEmail />
+            path: "/auth/signup/verify_email",
+            element: <VerifyEmail />,
+            loader: Loaders.emailVerification,
+            action: Actions.emailVerification
           },
           {
-            path: "/auth/update_your_profile",
-            element: <ProfileInfo />
+            path: "/auth/signup/register",
+            element: <NewAccountInfo />,
+            action: Actions.userRegistration,
           },
           {
-            path: "/auth/upload_your_profile_picture",
+            path: "/auth/signup/upload_your_profile_picture",
             element: <UploadProfilePicture />
           },
           {
