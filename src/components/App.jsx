796094a0968e010d7f324081190f6b04b91feb81
i9 <oluwarinolasam@gmail.com>
2024-02-02 15:00:40 +0100

    appInit loader data used

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,18 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLoaderData } from "react-router-dom"
+import { initClientUser } from "../lib/appState/slices/clientUserSlice"
+import { initHomeFeed } from "../lib/appState/slices/homeFeedSlice"
+import { useDispatch } from "react-redux"
+import { useCallback } from "react"
 
 export default function App() {
+  const loaderData = useLoaderData()
+  const dispatch = useDispatch()
+
+  useCallback(() => {
+    dispatch(initClientUser(loaderData.clientUser))
+    dispatch(initHomeFeed(loaderData.homeFeedPosts))
+  }, [dispatch, loaderData.clientUser, loaderData.homeFeedPosts])
+
   return (
     <div className="app--wrapper">
       <div className="tabs"></div>
